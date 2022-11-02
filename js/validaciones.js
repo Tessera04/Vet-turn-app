ea04d474e25efe4686cd6040379eefecb6995142
Matias Gonzalez <matute_11@hotmail.com.ar>
2022-11-02 05:06:50 -0300

    Validadores y puliendo funciones

--- js/validaciones.js
+++ js/validaciones.js
@@ -3,6 +3,30 @@ export function valida(input){
     if(validadores[tipoDeInput]){
         validadores[tipoDeInput](input)
     }
+
+    if(input.validity.valid){
+        input.parentElement.classList.remove("input-container--invalid");
+    }else{
+        input.parentElement.classList.add("input-container--invalid");
+    }
+}
+
+const mensajesError = {
+    nombre:{
+        valueMissing: "Este campo no puede estar vacio",
+    },
+    email:{
+        valueMissing: "Este campo no puede estar vacio",
+        typeMismatch: "El correo no es valido"
+    },
+    password:{
+        valueMissing: "Este campo no puede estar vacio",
+        paternMismatch: "Minimo seis caracteres, maximo 12, al menos una mayuscula, una minuscula y un numero: "
+    },
+    nacimiento: {
+        valueMissing: "Este campo no puede estar vacio",
+        customError: "Debes tener al menos 18 anos de edad"
+    }
 }
 
 const validadores = {
